b5a74c0a76784069ce28767fdb4301f39d3a395b
mitchellseifert <mitchellseifert@ku.edu>
2022-01-31 11:27:16 -0600

    organization

--- BattleshipArray.js
+++ BattleshipArray.js
@@ -1,4 +1,6 @@
 //building 10x10 Array for Attack Board
+//Logic for Array: 0=no ship, 1=ship, 2=hit ship
+
     var x = 10
     var y = 10
     var attackBoard = new Array()
@@ -11,24 +13,25 @@
     }
     console.log(attackBoard);
 
-
-//Logic for Array: 0=no ship, 1=ship, 2=hit ship
-
-//function for attacking ships to the Array
-attackShip()
+attackShip() //function for attacking ships to the Array
 function attackShip(){
-var inputRow = window.prompt("Enter the row you would like to attack: ");
-var inputColoumn = window.prompt("Enter the coloumn you would like to attack: ");
-inputRow = a
-inputColoumn = b
-let a = 0
-let b = 0
-if(attackBoard[a][b]=1)
-{
-    console.log("Hit!")
-    attackBoard[a][b]=2
-}
-else(console.log("Miss!"))
+    var inputRow = window.prompt("Enter the row you would like to attack: ");
+    var inputColoumn = window.prompt("Enter the coloumn you would like to attack: ");
+    let a = 0
+    let b = 0
+    inputRow = a
+    inputColoumn = b
+
+    if(attackBoard[a][b]=1) //attackBoard cannot be used and im not sure why
+    {
+      console.log("Hit!")
+      attackBoard[a][b]=2
+    }
+    else(console.log("Miss!"))
 }
 
-console.log("Mitchell and Git aren't getting along ;(")
+scanArray() //gets current value for specific array position
+function scanArray(){
+let temp = 0
+
+}
