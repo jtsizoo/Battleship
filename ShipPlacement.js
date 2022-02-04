92642e171ca84ee12d0b593e0077eaa04010bbc3
LiamMcKinney <ljmckinney02@gmail.com>
2022-02-04 10:45:19 -0600

    First draft of ShipPlacement.js Filled in initial versions of each function Must maintain as other files' interfaces are developed

--- ShipPlacement.js
+++ ShipPlacement.js
@@ -1,22 +1,27 @@
 let numShips = 5;
+let shipsRemaining = 0;
 let nextShip = {};
+let isP2 = false;
+shipList = [];
 
 //Call when the user chooses how many ships to play with.
 //Starts the ship placement phase of the game, beginning with player 1.
 function initializeShipPlacement(_numShips){
+    gameState = "p1Place";
+    isP2 = false;
     numShips = _numShips;
     shipsRemaining = numShips;
-    nextShip = {
-        length = shipsRemaining,
-        topLeft = a1,
-        isVertical = false
-    };
+    nextShip = initializeShip(shipsRemaining);
+    shipList = p1Ships;
 }
 
 //Helper function to begin placing ships for player 2.
 //Is called internally. Do not call from GraphicsUI.js
 function initializeP2Placement(){
+    gameState = "p2Place";
     switchWindow("p2view");
+    shipsRemaining = numShips;
+    nextShip = initializeShip(shipsRemaining);
 }
 
 //Call when the user hovers over one of their own cells.
@@ -37,16 +42,40 @@ function rotateShip(){
 //Will attempt to place a ship, add it to the ships list if valid, 
 //and move to the next stage of the game if no more ships need placed.
 function attemptShipPlace(cell){
+    moveShip(nextShip.length, cell, nextShip.isVertical);
+
     if(isShipValid(nextShip)){
-        
+        placeShip(nextShip);
+        shipsRemaining--;
+        if(shipsRemaining == 0){
+            if(isP2){
+                gameState = "p1Turn";
+                switchWindow("countdown");
+                //TODO call countdown function
+                switchWindow("p1view");
+            }else{
+                initializeP2Placement();
+            }
+        }else{
+            nextShip = initializeShip(shipsRemaining);
+        }
     }
 }
 
-//helper method to place the ship, update the ship list, and decrement the remaining ships.
-function placeShip(){}
-
-//Helper method to render a preview of the ship being placed
-function renderShipPreview(){}
+//helper method to place the ship and update the ship list.
+function placeShip(){
+    p1Ships.push(nextShip);
+    //set the ship to be gray and opaque
+    setShipProperties(nextShip.length, 1, "gray");
+}
 
 //Helper function to determine if a ship placement is valid.
 function isShipValid(){}
+
+function initializeShip(_length){
+    return {
+        length = _length,
+        topLeft = "a1",
+        isVertical = false
+    };
+}
