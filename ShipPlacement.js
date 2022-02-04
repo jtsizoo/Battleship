d7d02ab0c6ac772b0911a4b61788efb3f6a9bd58
LiamMcKinney <ljmckinney02@gmail.com>
2022-02-04 09:26:51 -0600

    Began input handlers for ship placement and added formatting to workEstimate.txt

--- ShipPlacement.js
+++ ShipPlacement.js
@@ -1,18 +1,46 @@
 let numShips = 5;
 let nextShip = {};
 
+//Call when the user chooses how many ships to play with.
+//Starts the ship placement phase of the game, beginning with player 1.
+function initializeShipPlacement(_numShips){
+    numShips = _numShips;
+    shipsRemaining = numShips;
+    nextShip = {
+        length = shipsRemaining,
+        topLeft = a1,
+        isVertical = false
+    };
+}
+
+//Helper function to begin placing ships for player 2.
+//Is called internally. Do not call from GraphicsUI.js
+function initializeP2Placement(){
+    switchWindow("p2view");
+}
+
 //Call when the user hovers over one of their own cells.
 //Will render a preview of the next ship to place. Red if invalid, Gray if valid.
-function hoverCell(cell){}
+function hoverCell(cell){
+    nextShip.topLeft = cell;
+    moveShip(nextShip.length, cell, nextShip.isVertical);
+}
 
 //Call when the user scrolls
 //Will rotate the ship between horizontal and vertical
-function rotateShip(){}
+function rotateShip(){
+    nextShip.isVertical = !nextShip.isVertical;
+    moveShip(nextShip.length, cell, nextShip.isVertical);
+}
 
 //Call when the user clicks a cell on their own board. 
 //Will attempt to place a ship, add it to the ships list if valid, 
 //and move to the next stage of the game if no more ships need placed.
-function attemptShipPlace(cell){}
+function attemptShipPlace(cell){
+    if(isShipValid(nextShip)){
+        
+    }
+}
 
 //helper method to place the ship, update the ship list, and decrement the remaining ships.
 function placeShip(){}
